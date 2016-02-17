be49e6fbb391a83de56272f61f1b3bb7c2964171
Nick Dunn <nickdunn2@gmail.com>
2016-02-16 19:28:44 -0600

    fix seeding issue - do not use sentences in  when model expects a string

--- app/Comment.php
+++ app/Comment.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+    ];
+
+    /**
      * Get the user a comment belongs to.
      */
     public function user() {
